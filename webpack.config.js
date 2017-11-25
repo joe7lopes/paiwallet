0632cb478fe6c51909eda791abe3590413694418
Joao Oliveira <j.andre.oliveira@gmail.com>
2017-11-25 18:48:20 +0100

    added hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/public');
-var APP_DIR = path.resolve(__dirname, 'src/app');
+var BUILD_DIR = path.resolve(__dirname, '/public/');
+var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
   entry: APP_DIR + '/index.js',
   output: {
     path: BUILD_DIR,
+    publicPath: BUILD_DIR,
     filename: 'bundle.js'
   },
 
