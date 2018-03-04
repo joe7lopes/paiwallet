ce8534ec1eac64c815446a4e60bfb008580f486b
Joao Oliveira <j.andre.oliveira@gmail.com>
2018-03-04 10:24:59 +0100

    removed build job.

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,8 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-if(process.env.NODE_ENV === 'test'){
+if(process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development'){
   require('dotenv').config({ path: '.env.test'});
-}else if(process.env.NODE_ENV === 'development' ){
-  require('dotenv').config({ path: '.env.development'});
 }
 
 module.exports = (env, argv) => {
