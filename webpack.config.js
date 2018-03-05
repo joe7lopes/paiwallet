38aa4d7f4b81b2547cf3b1dcd66fe2f63deb8cd3
Joao Oliveira <j.andre.oliveira@gmail.com>
2018-03-05 18:01:26 +0100

    removed debug lines. changes configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,8 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-if(process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development'){
+if(process.env.NODE_ENV === 'test-dev' || process.env.NODE_ENV === 'development'){
   require('dotenv').config({ path: '.env.test'});
-}else{
-  console.log("in webpack: ",process.env.NODE_ENV );
 }
 
 module.exports = (env, argv) => {
